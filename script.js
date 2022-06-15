cca7824abb9aa3c42c615b536480a27a17f15bef
LineMeo <aline.marqes@gmail.com>
2022-06-15 09:35:30 -0300

    3 desafio mod2 vnw

---
+++ script.js
@@ -0,0 +1,33 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+for (let count = 1; count <= 10; count++) {
+    console.log(count)
+}
+console.log('==========')
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+let contador = 10
+
+while (contador >= 1) {
+    console.log(contador)
+    contador--
+}
+
+console.log('==========')
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+let imp = 1
+
+while (imp <= 100) {
+    console.log(imp)
+    imp += 2
+}
+console.log('==========')
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+let par = 0
+
+while (par <= 100) {
+    console.log(par)
+    par += 2
+}
+console.log('==========')
